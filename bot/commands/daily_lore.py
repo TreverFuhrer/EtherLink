61c09e70d3bc5678d65e4fd30cf98a997ed32687
toki <clevertrever31@gmail.com>
2024-11-27 21:30:44 -0600

    Changed ai model name

--- bot/commands/daily_lore.py
+++ bot/commands/daily_lore.py
@@ -31,7 +31,7 @@ class LoreUpdate(commands.Cog):
 
         # Generate the lore update
         try:
-            client = InferenceClient(token=HUGGING_FACE_API_TOKEN, model="meta-llama/Llama-2-7b-Instruct")
+            client = InferenceClient(token=HUGGING_FACE_API_TOKEN, model="meta-llama/Llama-2-7b")
             lore_update = client.text_generation(
                 prompt,
                 max_new_tokens=300,  # Maximum tokens to generate
